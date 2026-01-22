503b5a69062ce0e4b920ab0909207cd85afb3bb8
mitchi <pg.saku0719@gmail.com>
2026-01-23 01:03:35 +0900

    station8

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
+require 'sqlite3'
+require './db/todos'
 
 get '/' do
   'Hello, World!'
 end
 
-require './db/todos'
 get '/todos' do
   @todos = DB.execute('SELECT * FROM todos')
   erb :todos
+end
+
+post '/todos' do
+  title = params['title']
+  DB.execute('INSERT INTO todos (title) VALUES (?)', [title])
+  redirect '/'
 end
